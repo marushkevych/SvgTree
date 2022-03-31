6cd6459dd30d7a5f4c026f02861ff81e69daace2
Marushkevych <marushkevych@gmail.com>
2022-03-31 13:49:38 -0400

    nested tree

--- src/SvgTree.js
+++ src/SvgTree.js
@@ -3,16 +3,43 @@ import './SvgTree.css';
 
 function SvgTree(props) {
   return (
-      <div className="tree">
+      <div>
         <div className="treeLabel">Root</div>
-        <div className="treeItem">
-          <LineIntersection/>
-          <div className="treeLabel">item 1</div>
-          <LineDrop/>
-        </div>
-        <div className="treeItem">
-          <LineIntersection/>
-          <div className="treeLabel">item 2</div>
+        <div className="tree">
+          <div className="treeItem">
+            <LineIntersection/>
+            <div className="treeLabel">item 1</div>
+            <LineDrop/>
+            <div className="tree">
+              <div className="treeItem">
+                <LineIntersection/>
+                <div className="treeLabel">item 1a</div>
+                <LineDrop/>
+                <div className="tree">
+                  <div className="treeItem">
+                    <LineIntersection/>
+                    <div className="treeLabel">item 1aa</div>
+                    <LineDrop/>
+                  </div>
+                  <div className="treeItem">
+                    <LineIntersection/>
+                    <div className="treeLabel">item 1ab</div>
+                    <LineDrop/>
+                  </div>
+                </div>
+              </div>
+              <div className="treeItem">
+                <LineIntersection/>
+                <div className="treeLabel">item 1b</div>
+                <LineDrop/>
+              </div>
+            </div>
+          </div>
+          <div className="treeItem">
+            <LineIntersection/>
+            <div className="treeLabel">item 2</div>
+            <LineDrop/>
+          </div>
         </div>
       </div>
   );
@@ -31,7 +58,7 @@ function LineIntersection() {
 
 function LineDrop() {
   return (
-      <svg width="40" height="100" viewBox="0 0 40 1" preserveAspectRatio="none" className="line-drop">
+      <svg width="40" height="100%" viewBox="0 0 40 1" preserveAspectRatio="none" className="line-drop">
         <line x1="16" x2="16" y2="1"/>
       </svg>
   );
